8ca3d59c34d1f080105c057aa58132f56b619868
Alex Wang <filfirst@gmail.com>
2013-12-15 17:00:53 +0800

    Initial commit

---
+++ Sources/FTKJson/JsonObject.cpp
@@ -0,0 +1,18 @@
+//
+//  JsonObject.cpp
+//  FTKJson
+//
+//  Created by Alex Wang on 2013/12/15.
+//  Copyright (c) 2013 FTK. All rights reserved.
+//
+
+#include "JsonObject.h"
+
+
+namespace ftk {
+namespace utility {
+
+    
+} // namespace utility
+} // namespace ftk
+
---
+++ Sources/FTKJson/JsonObject.h
@@ -0,0 +1,29 @@
+//
+//  JsonObject.h
+//  FTKJson
+//
+//  Created by Alex Wang on 2013/12/15.
+//  Copyright (c) 2013 FTK. All rights reserved.
+//
+
+#ifndef FTK_JSON_JSON_OBJECT_H_
+#define FTK_JSON_JSON_OBJECT_H_
+
+
+namespace ftk {
+namespace utility {
+
+
+class JsonObject {
+public:
+    JsonObject() = default;
+    ~JsonObject() = default;
+};
+
+
+} // namespace utility
+} // namespace ftk
+
+
+#endif // FTK_JSON_JSON_OBJECT_H_
+
